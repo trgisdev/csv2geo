734e47dfafc75c837294f54817c13404931afda4
Onurhan Şeremet <140516908+seremetonurhan@users.noreply.github.com>
2024-02-24 16:34:28 +0200

    Main

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
+
 # csv2geo
-It enables you to convert files in CSV format into geographic datasets and visualize them on a map
+
+It enables you to convert files in CSV format into geographic datasets
+
+##  
+
+
+
+```bash 
+  npm install csv2geo
+```
+    
+## Example
+
+```javascript
+const csv2geo = require('csv2geo')
+
+csv2geo.geojson('./example.csv')
+.then((res)=>{
+    
+    let geojsonObject   = res
+    let geojsonFeatures = res.features
+    
+})
+.catch((err)=>{
+    console.log(err)
+})
+```
+
+  
---
+++ index.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const { parse } = require("csv-parse");
+const { rejects } = require('assert');
+
+const possibleLongitudeNames = [
+    'lon',
+    'long',
+    'longitude',
+    'Y',
+    'boylam',
+]
+
+const possibleLatitudeNames = [
+    'lat',
+    'latitude',
+    'X',
+    'enlem'
+]
+
+
+
+
+
+class csv2geo {
+
+    geojson = (csvFile,options) => new Promise((resolve,reject)=>{
+        let csvArr      = []
+        let geojson     =   {
+                                "type": "FeatureCollection",
+                                "features": []
+                            }
+
+        fs.createReadStream(csvFile)
+        .pipe(parse({ delimiter: ",", from_line: 1 }))
+        .on("data", function (row) {
+            csvArr.push(row)
+        })
+        .on("end", function () {
+            if(options == undefined){
+                
+                let fieldNames = csvArr[0]
+                let lonIndex
+                let latIndex
+    
+                for (let i = 0; i < fieldNames.length; i++) {
+                    let currentName = fieldNames[i].toLowerCase();
+                    
+                    if(possibleLongitudeNames.includes(currentName))
+                        lonIndex = fieldNames.indexOf(fieldNames[i])
+                    if(possibleLatitudeNames.includes(currentName))
+                        latIndex = fieldNames.indexOf(fieldNames[i])
+                }
+
+                if(lonIndex == undefined || latIndex == undefined){
+                    reject("Error while find Lon-lat Values. Please use options parameter. doc:https://github.com/trgisdev/csv2geo");
+                    return
+                }
+
+                // Remove field names
+                csvArr.shift()
+
+                for (let i = 0; i < csvArr.length; i++) {
+                    let currentRow = csvArr[i]
+                    let lat = currentRow[latIndex]
+                    let lon = currentRow[lonIndex]
+
+                    fieldNames.splice(lonIndex,1)
+                    fieldNames.splice(latIndex,1)
+                    
+                    let currentGeojson = {
+                            "type": "Feature",
+                            "geometry": {
+                            "type": "Point",
+                            "coordinates": [lon,lat]
+                        },
+                            "properties": currentRow.map((d,i)=>{return {[fieldNames[i]]:d}})
+                        
+                    }
+
+                    geojson.features.push(currentGeojson)
+
+                    
+                }
+                resolve(geojson)
+
+
+
+
+
+            }
+        })
+        .on("error", function (error) {
+            reject("Error while read CSV file. doc:https://github.com/trgisdev/csv2geo ",error);
+        });
+        return
+
+    })
+        
+    
+  }
+
+
+  module.exports = new csv2geo;
